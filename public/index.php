346213f350994ba2fe14a05687db8bb0cf59ef5b
Yoann <yoann.gloaguen@gmail.com>
2016-10-07 08:29:08 +0200

    adding voting capacities every values increment or decrement when necessary, name hide when vote count rich 0

--- public/index.php
+++ public/index.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Concours WCS</title>
-    <meta charset="utf-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="stylesheet.css"/>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <?php include 'head.php' ?>
 </head>
 <body>
     <header>
@@ -27,9 +16,9 @@
                 Voteleft: <input type="text" name="Voteleft" value="">
                 Tovote: <input type="text" name="Tovote" value="">
                 <input type="submit" value="Submit">
-
             </form>
-
+        </div>
+        <div class="row">
         </div>
     </div>
 </body>
